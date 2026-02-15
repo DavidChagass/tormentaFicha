7197138387bffcda3bb820c588d02db1f653e7f8
DavidChagass <david.teodoro.vale.chagas@gmail.com>
2026-02-15 12:24:12 -0300

    add/modal-exclusao

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,36 +7,63 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="icon" href="{{ asset('IconTormenta20.ico') }}">
     <script src="https://cdn.tailwindcss.com"></script>
-
+    <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
     <title>Dashboard</title>
 </head>
 
-<body class="bg-[#fdf6e3]">
+<!-- ADICIONADO O X-DATA ABAIXO PARA O ALPINE FUNCIONAR -->
+<body class="bg-[#fdf6e3]" x-data="{ showModal: false, charName: '', formId: '' }">
     <div class="mt-12"></div>
     <a class="bg-red-800 text-white p-2 rounded m-4 " href="{{ route('personagem.create') }}">
         Criar personagem
     </a>
-    <div class=" ">
-
-    </div>
+    
     <ul class="ml-4 mt-8">
         @foreach ($personagens as $p)
-            <div class="border-red-800 border border-2 w-fit h-fit p-4 rounded">
+            <div class="border-red-800 border border-2 w-fit h-fit p-4 rounded mb-4">
                 <a href="{{ route('personagem.show', $p->id) }}">
-                    <li class="rounded text-white my-2 p-4 w-80 bg-red-800 rounded">
+                    <li class="rounded text-white my-2 p-4 w-80 bg-red-800">
                         {{ $p->nome }} - nivel {{ $p->nivel }} - {{ $p->classe }}
                     </li>
                 </a>
-                <form action="{{ route('delete.personagem', $p->id) }}" method="POST">
+                <form id="delete-form-{{ $p->id }}" action="{{ route('delete.personagem', $p->id) }}"
+                    method="POST">
                     @csrf
                     @method('DELETE')
-                    <button class="mt-16 bg-blue-200 w-fit p-4 rounded" type="submit">deletar:
-                        {{ $p->nome }}</button>
+                    <button
+                        @click.prevent="showModal = true; charName = '{{ $p->nome }}'; formId = 'delete-form-{{ $p->id }}'"
+                        class="mt-16 bg-blue-200 w-fit p-4 rounded hover:bg-red-200 transition" type="button">
+                        deletar: {{ $p->nome }}
+                    </button>
                 </form>
             </div>
         @endforeach
-        <hr class="border  border-gray-700 my-2 w-96">
+        <hr class="border border-gray-700 my-2 w-96">
     </ul>
-</body>
 
+    <!-- MODAL -->
+    <div x-show="showModal"
+        class="fixed inset-0 z-50 flex items-center justify-center bg-black/60 backdrop-blur-sm p-4"
+        style="display: none;"> <!-- O Alpine remove esse display:none automaticamente -->
+
+        <div class="bg-[#fdf6e3] border-4 border-red-800 p-8 rounded shadow-2xl max-w-sm w-full text-center">
+            <h2 class="text-xl font-black text-red-800 uppercase mb-4">Confirmar Exclusão</h2>
+
+            <p class="text-gray-800 mb-6">
+                Você tem certeza que deseja apagar o personagem <br>
+                <span class="font-bold text-lg"><span x-text="charName"></span>?
+            </p>
+
+            <div class="flex gap-4">
+                <button @click="showModal = false" class="flex-1 px-4 py-2 border border-gray-400 rounded hover:bg-gray-100 transition">
+                    SIM
+                </button>
+
+                <button @click="document.getElementById(formId).submit()" class="flex-1 px-4 py-2 bg-red-800 text-white font-bold rounded hover:bg-red-900 transition"> 
+                    SIM
+                </button>
+            </div>
+        </div>
+    </div>
+</body>
 </html>
